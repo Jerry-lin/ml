88d21da9d2a53b15a978c04fc084f84274822254
Jerry-lin <jerry.lin3000@gmail.com>
2016-12-05 07:30:07 -0500

    Update observer_test_kmeans.m

--- cluster/src/main/matlab/observer_test_kmeans.m
+++ cluster/src/main/matlab/observer_test_kmeans.m
@@ -1,9 +1,9 @@
 function observer_test(data, iters, Z, C)    
-%     figure
-%     N = 150;
-%     scatter3(data(1,:),data(2,:),data(3,:),50*ones(1,N),[Z; zeros(1,N)]');   
-%     hold on; scatter3(C(1,:),C(2,:),C(3,:),'bx');
-%     k=waitforbuttonpress
+    figure
+    N = size(data, 2);
+    scatter3(data(1,:),data(2,:),data(3,:),50*ones(1,N),[Z; zeros(1,N)]');   
+    hold on; scatter3(C(1,:),C(2,:),C(3,:),'bx');
+    waitforbuttonpress;
 
 % disp(iters);
 % disp(Z);
